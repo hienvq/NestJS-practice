0efa04f1b33e34b8a13cf03396075af81f5fc022
HienVQ <vuhien96@gmail.com>
2020-04-26 19:45:15 +0700

    create simple CRUD api with validation and error handing

---
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -0,0 +1,16 @@
+import { TaskStatus } from "../tasks.model";
+import { IsOptional, IsIn, IsNotEmpty } from "class-validator";
+
+export class GetTasksFilterDto {
+  @IsOptional()
+  @IsIn([
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGESS,
+    TaskStatus.DONE,
+  ])
+  status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
+  search: string;
+}
---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,21 @@
+import { PipeTransform, BadRequestException } from "@nestjs/common";
+import { TaskStatus } from "../tasks.model";
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGESS,
+    TaskStatus.DONE,
+  ];
+  transform(value: any) {
+    value = value.toUpperCase();
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`"${value}" isnot valid`);
+    }
+    return value;
+  }
+  private isStatusValid(status: any) {
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
+  }
+}
